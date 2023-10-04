eab1d621367f896c3d1914e8811c881c58eb54a3
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-10-04 09:13:16 -0300

    COMPLETED NODE EXERCISES

--- node/ex06_lodash.js
+++ node/ex06_lodash.js
@@ -1,1 +1,16 @@
-const _ =require('lodash')
+const _ = require('lodash')
+
+const alunos = [{
+    nome: 'joao',
+    nota: 7.6
+},{
+    nome: 'maria',
+    nota: 8.6
+},{
+    nome: 'fernando',
+    nota: 6.1
+}]
+
+const media = _.sumBy(alunos,'nota') /alunos.length
+
+console.log(media)
